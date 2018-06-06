f026a08990ece72dcd3bd6ab7908c61ff434e3c0
wjm <1186619054@qq.com>
2018-06-06 10:14:05 +0800

    取消鼠标点击跳转

--- js/app.js
+++ js/app.js
@@ -76,7 +76,7 @@ function init() {
     // 监听键盘按键
     bindKeyboard(hash)
     // 鼠标点击
-    bindKbdClick(hash)
+    // bindKbdClick(hash)
     // 悬浮提示网址
     bindKbdHover(hash)
     // 修改导航网址
@@ -141,16 +141,16 @@ function bindKeyboard(hash) {
     }, false)
 }
 // 鼠标点击
-function bindKbdClick(hash) {
-    let kbds = getElementsBySelector('.wrapper kbd')
-    kbds.forEach((kbd) => {
-        kbd.addEventListener('click',(e) => {
-            let key = kbd.childNodes[1].textContent
-            let website = hash[key]
-            window.open(`http://${website}`, '_blank')
-        },false)
-    })
-}
+// function bindKbdClick(hash) {
+//     let kbds = getElementsBySelector('.wrapper kbd')
+//     kbds.forEach((kbd) => {
+//         kbd.addEventListener('click',(e) => {
+//             let key = kbd.childNodes[1].textContent
+//             let website = hash[key]
+//             window.open(`http://${website}`, '_blank')
+//         },false)
+//     })
+// }
 
 // 修改导航网址
 function bindEdit(hash) {
